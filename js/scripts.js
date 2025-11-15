24e3515c6419a991052d9de25b4dc8668d75d8b9
Felipe <elann.360@hotmail.com>
2025-11-14 23:28:21 -0500

    Images

--- js/scripts.js
+++ js/scripts.js
@@ -95,13 +95,14 @@
             delay: 2000,
             disableOnInteraction: false
 		},
-        loop: false,
+        loop: true,
         navigation: {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
 		},
-        spaceBetween: 30,
-        slidesPerView: 5,
+        centeredSlides: true,
+        spaceBetween: 50,
+        slidesPerView: 1,
 		breakpoints: {
             // when window is <= 380px
             380: {
@@ -110,23 +111,23 @@
             },
             // when window is <= 516px
             516: {
-                slidesPerView: 2,
-                spaceBetween: 10
+                slidesPerView: 1,
+                spaceBetween: 20
             },
             // when window is <= 768px
             768: {
-                slidesPerView: 3,
-                spaceBetween: 20
+                slidesPerView: 1,
+                spaceBetween: 30
             },
             // when window is <= 992px
             992: {
-                slidesPerView: 4,
-                spaceBetween: 30
+                slidesPerView: 1,
+                spaceBetween: 50
             },
             // when window is <= 1200px
             1200: {
-                slidesPerView: 5,
-                spaceBetween: 30
+                slidesPerView: 1,
+                spaceBetween: 50
             },
         }
     });
